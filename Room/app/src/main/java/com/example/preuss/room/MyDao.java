0bec448838192e379cc6129810bb444ef2bc5f12
Preuss Pierre <preuss@a10p16.fil.univ-lille1.fr>
2018-11-16 19:58:48 +0100

    moitié du tuto

---
+++ Room/app/src/main/java/com/example/preuss/room/MyDao.java
@@ -0,0 +1,14 @@
+package com.example.preuss.room;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+
+@Dao
+public interface MyDao
+{
+
+    @Insert
+    public void addUser(User user);
+
+}
+
